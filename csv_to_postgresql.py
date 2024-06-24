57a3a41e8c8c2cc21392c17ae38d4de720b6c3af
Joann Feng <joannf310@gmail.com>
2024-06-23 22:32:32 -0500

    update README formatting, add comments to python script

--- csv_to_postgresql.py
+++ csv_to_postgresql.py
@@ -1,5 +1,6 @@
 import psycopg2 
-  
+
+# provide database details for connection
 conn = psycopg2.connect(database="ECOMMERCE_DATABASE", 
                         user='postgres', password='password',  
                         host='127.0.0.1', port='5432'
@@ -7,7 +8,8 @@ conn = psycopg2.connect(database="ECOMMERCE_DATABASE",
   
 conn.autocommit = True
 cursor = conn.cursor() 
-  
+
+# table creation using csv column headers
 sql = '''CREATE TABLE PET_SUPPLIES(title varchar(500),
 averageStar float,
 quantity int NOT NULL,
@@ -16,7 +18,8 @@ tradeAmount varchar(30), wishedCount int NOT NULL);'''
 # sql = '''DROP TABLE PET_SUPPLIES;'''
 
 cursor.execute(sql) 
-  
+
+# copy csv into table  
 sql2 = '''COPY PET_SUPPLIES(title,averageStar,
 quantity,tradeAmount,wishedCount) 
 FROM 'C:/Users/joann/OneDrive/Desktop/projects/js-react/aliexpress_pet_supplies.csv' 
@@ -24,7 +27,8 @@ DELIMITER ','
 CSV HEADER;'''
   
 cursor.execute(sql2) 
-  
+
+# display table details once complete  
 sql3 = '''SELECT * FROM PET_SUPPLIES;'''
 cursor.execute(sql3) 
 for i in cursor.fetchall(): 
